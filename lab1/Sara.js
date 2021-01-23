82674581e5fa4e66c4434fa238b143d84bd441b5
saraecheverria <76416538+saraecheverria@users.noreply.github.com>
2021-01-22 21:59:32 -0500

    lab1

---
+++ lab1/Sara.js
@@ -0,0 +1,7 @@
+import Person from './Person.js'; 
+
+class Sara extends Person{ 
+    constructor() { 
+        super ("Sara", '#DCEAF4'); 
+    }
+}
---
+++ lab1/index.js
@@ -0,0 +1,4 @@
+import Person from './Person.js'; 
+
+const p = new Person ('Sara', '#DCEAF4'); 
+p.speak(); 
