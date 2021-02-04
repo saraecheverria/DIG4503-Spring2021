629d8b64abaf0bb031de4ab199883a4a9f60f2ef
saraecheverria <76416538+saraecheverria@users.noreply.github.com>
2021-02-04 16:28:43 -0500

    lab3

---
+++ lab3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import HomePage from './Components/HomePage'; 
+
+function App() { 
+    return ( 
+        <div className="App"> 
+        <HomePage firstName='Sara' /> 
+        </div> 
+    ); 
+}
+
+export default App; 
